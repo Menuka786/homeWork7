6c36d60ab87fac64babd31e403d34a32bb8107de
Menuka Gusain <menukagusain@gmail.com>
2021-01-17 23:03:20 -0500

    added questions

---
+++ Develop copy/HWREADME.md
@@ -0,0 +1,5 @@
+{
+	"projectTitle": "Read me Generator",
+	"description": "this is a read me generator",
+	"contact": "email"
+}
--- Develop copy/index.js
+++ Develop copy/index.js
@@ -4,48 +4,100 @@
 
 const inquirer = require('inquirer');
 const fs = require('fs');
-const fileContent = " README Generator";
-const filepath = "hw7Readme.md";
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-//const questions = [];
-
-// TODO: Create a function to write README file
-//function writeToFile(fileName, data) {}
-
-// TODO: Create a function to initialize app
-//function init() {}
-
-// Function call to initialize app
 
 
 inquirer
   .prompt([
+
+    //Project Title 
     {
       type: 'input',
-      name: 'projectTitle',
+      name: 'ProjectTitle',
       message: 'Enter your project title',
     },
+
+    //Read me Description
     {
       type: 'input',
-      name: 'description',
+      name: 'Description',
       message: 'Write a brief description of your project',
      
     },
+    //Installation instruction 
     {
-      type: 'list',
-      message: 'What is your preferred method of communication?',
-      name: 'contact',
-      choices: ['email', 'phone', 'telekinesis'],
+      type: 'input',
+      
+      name: 'Installation Instruction ',
+      message: 'Type in your installation instruction?',
+    
     },
+    // Usage information 
+    {
+      type: 'input',
+      
+      name: 'Usage ',
+      message: 'Please write usage information ',
+    
+    },
+
+    //Input
+    {
+      type: 'input',
+      
+      name: 'Contribution Guidelines ',
+      message: 'Please write Contribution Guidelines ',
+    
+    },
+    {
+      type: 'input',
+      
+      name: 'Test instruction  ',
+      message: 'Are there any test instruction ? ',
+    
+    },
+    {
+      type: 'input',
+      name: 'Application License  ',
+      message: 'Choose your Licence ',
+      choices :['License 1', 'License 2','License 3']
+    
+    },
+    {
+type:"input",
+name :"questions",
+message: "Enter your github username.",
+
+    },
+    
+    {
+    
+    type : "input",
+    name: "LinkedIn",
+    message: 'Enter your LinedIn URL.',
+
+
+    },
+    
+    {
+    type : "input",
+    name: "Email",
+    message: 'Enter your Email Address',
+
+
+    },
+
   ])
+  .then((data) => {
 
-
-
-  fs.writeFile(filepath, fileContent, (err) => {
-    if (err) throw err;
-    console.log("The file was succesfully saved!");
-
+    
+    
+    fs.writeFile("HWREADME.md", generateMarkdown(data));
+ 
+console.log ("success");
 
 
   });
