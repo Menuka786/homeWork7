1d8e07f7a5f1bcfda4d0ee8b225a52cb46e3c897
Menuka Gusain <menukagusain@gmail.com>
2021-01-30 22:13:22 -0500

    updated generatemarkdown file

---
+++ Develop copy/utils/README.md
@@ -0,0 +1,37 @@
+# fdfjalf
+
+ * [License](#license)
+
+## Description
+dfjdlfdlf
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing) 
+* [Tests](#tests)
+* [Questions](#questions) 
+## Installation
+To install necessary dependencies, run the following command:
+```
+lfjldfldf
+```
+## Usage
+lfjldfjalfj
+## License
+      
+  This project is licensed under the MIT license.
+## Contributing
+fdfldjf
+## Tests
+To run tests, run the following command:
+```
+kfdkfljdfl
+```
+## License
+undefined
+## Questions
+If you have any questions contact me directly at fkjdlfal. 
+You can find more of my work at [fldfadfjl](http://github.com/fldfadfjl/).
---
+++ Develop copy/utils/index.js
@@ -0,0 +1,71 @@
+// TODO: Include packages needed for this application
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path =require('path')
+const generateMarkdown = require('./generateMarkdown')
+
+const questions = [
+  
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is your project name?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please write a short description of your project',
+      
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',    
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?',    
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo?',    
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?',    
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'What kind of license should your project have?',  
+        choices: ['MIT','APACHE 5.0', 'GPL 2.0', 'NIT 3', 'None']  
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your GitHub username?',    
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address',    
+    },
+ 
+  ];
+  // TODO: Create a function to write README file
+    function writeToFile(fileName, data) {
+        return fs.writeFileSync(path.join(process.cwd(),fileName),data);
+    }
+    
+    function init(){
+        inquirer.prompt(questions).then((inquirerResponses) => {
+            console.log('Generating README ..');
+            writeToFile('README.md', generateMarkdown({...inquirerResponses}));
+        });
+    }
+
+  init();
